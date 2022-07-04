cdf257f45f04d7723b7346875feb5ccb2530e469
KATIOIIIA <98949682+KATIOIIIA@users.noreply.github.com>
2022-07-04 23:40:25 +0300

    Update StyleTransferModel.py

--- StyleTransferModel.py
+++ StyleTransferModel.py
@@ -40,8 +40,8 @@ class StyleTransferModel(object):
 
         self.cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(self.device)
         self.cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(self.device)
-        self.cnn = models.vgg19(pretrained=True).features.to(self.device).eval()
-        # self.cnn = torch.load('model/vgg19.pth').to(self.device).eval()
+        # self.cnn = models.vgg19(pretrained=True).features.to(self.device).eval()
+        self.cnn = torch.load('model/vgg19.pth').to(self.device).eval()
 
     def image_loader(self, download_file):
         image = Image.open(download_file)
