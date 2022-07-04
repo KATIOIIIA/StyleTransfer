8bd8ec9eca2c5b78870b1f8a9284672b0d7779aa
KATIOIIIA <98949682+KATIOIIIA@users.noreply.github.com>
2022-07-04 13:25:34 +0300

    Update Normalization.py

--- Normalization.py
+++ Normalization.py
@@ -1,5 +1,5 @@
-import torch
 import torch.nn as nn
+from torch import tensor
 
 
 class Normalization(nn.Module):
@@ -8,8 +8,8 @@ class Normalization(nn.Module):
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = tensor(mean).view(-1, 1, 1)
+        self.std = tensor(std).view(-1, 1, 1)
 
     def forward(self, img):
         # normalize img
