8f28291899b1c013f6c0db8bcf951bafcdfe175e
KATIOIIIA <98949682+KATIOIIIA@users.noreply.github.com>
2022-07-04 13:26:20 +0300

    Update StyleLoss.py

--- StyleLoss.py
+++ StyleLoss.py
@@ -1,7 +1,6 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch import mm
 
 def gram_matrix(input):
     batch_size, h, w, f_map_num = input.size()  # batch size(=1)
@@ -10,7 +9,7 @@ def gram_matrix(input):
 
     features = input.view(batch_size * h, w * f_map_num)  # resise F_XL into \hat F_XL
 
-    G = torch.mm(features, features.t())  # compute the gram product
+    G = mm(features, features.t())  # compute the gram product
 
     # we 'normalize' the values of the gram matrix
     # by dividing by the number of element in each feature maps.
